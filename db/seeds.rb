b4417d44080b72fb54a9185f146cb177ae3a08ff
ao21 <lpubnrm.ay@gmail.com>
2021-02-08 09:30:03 +0900

    初期データの内容を修正しデータベースへ投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ Post.destroy_all
 
 user1 = User.create!(email: "satou@example.com", password: "password")
 user2 = User.create!(email: "suzuki@example.com", password: "password")
-user1 = User.create!(email: "tanaka@example.com", password: "password")
+user3 = User.create!(email: "tanaka@example.com", password: "password")
 
 Post.create!(content: "おはよう", user_id: user2.id)
 Post.create!(content: "こんにちは", user_id: user3.id)
